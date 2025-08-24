379ece46e262d6ce97f4cb7fd8867103305b64c9
Fede Rudiero <federudiero@gmail.com>
2025-08-24 12:07:20 -0300

    frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/ver" replace />} />
-        <Route path="/ver" element={<App />} />
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
